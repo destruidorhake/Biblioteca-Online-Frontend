30d64cd0631b41d3689a57d0f2dedd5742c25f6a
GUSTAVO GOMES MARQUES <gustavomarques178@hotmail.com>
2024-10-31 16:15:29 -0300

    ajuste pesquisar

--- src/app/Authentication/environment.ts
+++ src/app/Authentication/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   local: 'https://biblioteca-online-backend-production.up.railway.app'
   //Use LocalHost para desenvolvimento e testes
-  //local: 'http://localhost:8080'
+  //  local: 'http://localhost:8080'
 };
