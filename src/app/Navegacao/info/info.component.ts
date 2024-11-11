2380923f7862d8a9c1891bcc48233fd22a296236
GUSTAVO GOMES MARQUES <gustavomarques178@hotmail.com>
2024-11-11 16:39:33 -0300

    ajuste sumir seta

--- src/app/Navegacao/info/info.component.ts
+++ src/app/Navegacao/info/info.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './info.component.html',
   styleUrl: './info.component.css'
 })
 export class InfoComponent {
+  isMobile: boolean = false;
+
+  ngOnInit(): void {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize(): void {
+    this.isMobile = window.innerWidth <= 768;
+  }
 
   scrollDown() {
     window.scrollBy({
